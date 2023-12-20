fcef8bffab1d8550ee3b52600cd87b37ae7378f4
Ketan <royketan09@gmail.com>
2023-12-20 23:43:52 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let text1="Join our community and find your calling";
+let array_text=text1.split("");
+let looptimer;
+
+function text_printer(){
+    if(array_text.length>0){
+        document.getElementById("Appearing-text").innerHTML+=array_text.shift();
+    }
+    else{
+        clearTimeout(looptimer);
+        return false;
+    }
+    looptimer=setTimeout('text_printer()',40)
+}
+
+text_printer()
+
+
+console.log(array_text)
