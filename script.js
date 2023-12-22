45cfb95bacb43035c97ada5199e6495fd4bb8e3c
MrRoy09 <royketan09@gmail.com>
2023-12-22 23:38:03 +0530

    Added transition between login/signup

--- script.js
+++ script.js
@@ -13,7 +13,36 @@ function text_printer(){
     looptimer=setTimeout('text_printer()',40)
 }
 
+function toggle_display1(){
+    const login= document.getElementsByClassName('login-form-wrapper')[0];
+    const signup=document.getElementsByClassName('signup-form-wrapper')[0];
+    if(login.getAttribute("class")=='login-form-wrapper show'){
+        login.setAttribute("class","login-form-wrapper hide")
+        signup.setAttribute("class","signup-form-wrapper show")
+        const timeout=setTimeout(change_display,300,0)
+    }
+    else if(login.getAttribute("class")=='login-form-wrapper hide'){
+        login.setAttribute("class","login-form-wrapper show")
+        signup.setAttribute("class","signup-form-wrapper hide")
+        const timeout=setTimeout(change_display,300,1)
+    }
+    
+}
+
+function change_display(toggle){
+    const login= document.getElementsByClassName('login-form-wrapper')[0];
+    const signup=document.getElementsByClassName('signup-form-wrapper')[0];
+    if(toggle==0){
+        login.style.display='none';
+        signup.style.display='block';
+    }
+    else if (toggle==1){
+        login.style.display='block';
+        signup.style.display='none';
+    }
+    
+    
+}
+
+
 text_printer()
-
-
-console.log(array_text)
